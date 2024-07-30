73428f91f75b6ba53121132618e61d96655c7220
Your Name <you@example.com>
2024-07-30 15:28:33 +0000

    initial auth service

---
+++ auth/app/api/main.py
@@ -0,0 +1,37 @@
+from sqlite3 import IntegrityError
+from fastapi import Depends, FastAPI, HTTPException, Request
+from starlette.middleware.cors import CORSMiddleware
+from app.api.schemas.user import UserRead
+from app.settings import settings
+from .routers import api_router
+from sqlalchemy.exc import IntegrityError
+from .authorization.func import get_current_user
+
+
+
+app = FastAPI(
+    title=settings.PROJECT_TITLE, 
+    version="1.0.0",
+    root_path=settings.FAST_API_PREFIX
+    )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.exception_handler(IntegrityError)
+async def integrity_error_handler(request: Request, exc: IntegrityError):
+    raise HTTPException(status_code=400, detail=str(exc.orig).split("\nDETAIL:  ")[1])
+
+
+app.include_router(api_router)
+
+@app.get("/GET", response_model=UserRead)
+async def get(request: Request, user: UserRead = Depends(get_current_user)):
+    return user
---
+++ auth/app/db/models/user.py
@@ -0,0 +1,31 @@
+from typing import Optional
+import uuid
+from sqlalchemy import (
+    Computed,
+    String,
+    text,
+    UniqueConstraint,
+    func,
+    UUID,
+    ForeignKey,
+    SMALLINT,
+    Boolean,
+    ARRAY,
+)
+from sqlalchemy.dialects.postgresql import BYTEA
+import uuid
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import JSONB
+from app.db.base import Base
+from sqlalchemy_utils import UUIDType
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[str] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    username: Mapped[str] = mapped_column(String(255), unique=True)
+    password: Mapped[BYTEA] = mapped_column(type_=BYTEA(1024))
+    surname: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(String(255))
+    patronymic: Mapped[Optional[str]] = mapped_column(String(255))
+    is_archived: Mapped[bool] = mapped_column(Boolean, server_default='false')
